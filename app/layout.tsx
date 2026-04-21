aaa23e0260af3e1f6ce8acafae4c188112321acd
Hakam-7-dev <hakam.us.hammoud@gmail.com>
2026-04-22 01:53:24 +0400

    Fixing up the Deployment Issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from "next";
-import { Geist } from "next/font/google";
+import { Geist} from "next/font/google";
+import { Metadata } from "next";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
